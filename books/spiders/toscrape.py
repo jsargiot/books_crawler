73c0ced7b3c025aa40e6b68112e5acec2ab0d2c0
Joaquin Sargiotto <joaquinsargiotto@gmail.com>
2021-08-11 17:58:08 -0300

    do https

--- books/spiders/toscrape.py
+++ books/spiders/toscrape.py
@@ -5,7 +5,7 @@ class ToscrapeSpider(scrapy.Spider):
     name = "toscrape"
     allowed_domains = ["books.toscrape.com"]
     start_urls = [
-        'http://books.toscrape.com/',
+        'https://books.toscrape.com/',
     ]
 
     def parse(self, response):
